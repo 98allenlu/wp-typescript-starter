1da05bba7adbc7bb25224be619dc0acfad0c2c3b
98allenlu <85421725+98allenlu@users.noreply.github.com>
2026-07-12 18:54:13 -0400

    new changes

--- assets/public/main.tsx
+++ assets/public/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import App from './src/app/App';
 
-// Import your global CSS here
-import '../../../default_shadcn_theme.css';
+// Import your global CSS here (up two levels to the root)
+import '../../default_shadcn_theme.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
